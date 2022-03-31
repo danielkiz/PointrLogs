2e3b6897932d0ae9bb3fee3f7643d4405bc69ca6
Daniel Kiz <danylo.kiz@gmail.com>
2022-04-01 00:53:09 +0200

    Completed the work on the Logging file

--- PointrLogs/Logging.swift
+++ PointrLogs/Logging.swift
@@ -9,4 +9,30 @@ import Foundation
 
 class Logging {
     
+    /// Variable responsible for handling the logs
+    var logs: [String] = []
+    
+    /// Function for writing data to our "logs" variable
+    func write(singleLog: String) {
+        let _logs: [String] = {
+            if logs.count == 0 {
+                return [singleLog]
+            } else {
+                logs.append(singleLog)
+                return logs
+            }
+        }()
+        logs = _logs
+    }
+    
+    /// Responsible for printing out the logs
+    func print() -> [String] {
+        return logs
+    }
+    
+    /// Function which deletes all the logs
+    func clear() {
+        logs.removeAll()
+    }
+    
 }
