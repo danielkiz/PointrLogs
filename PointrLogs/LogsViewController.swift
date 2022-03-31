7b88fddf3815278611af65c3a1fc62df27b8d05c
Daniel Kiz <danylo.kiz@gmail.com>
2022-03-31 21:50:23 +0200

    Created a new UIViewController file called LogsViewController

---
+++ PointrLogs/LogsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LogsViewController.swift
+//  PointrLogs
+//
+//  Created by Daniel on 3/31/22.
+//
+
+import UIKit
+
+class LogsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
