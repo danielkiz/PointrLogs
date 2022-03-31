92a45bccad4c503c191f4789bd866efb362c8b30
Daniel Kiz <danylo.kiz@gmail.com>
2022-03-31 22:23:52 +0200

    Creating the visual part of the app in Storyboard and adding the IBOutlets and IBActions to the LogsViewController file

--- PointrLogs/LogsViewController.swift
+++ PointrLogs/LogsViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class LogsViewController: UIViewController {
-
+    
+    @IBOutlet weak var logsView: UIView!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
+    /// Action responsible for creating application logs
+    @IBAction func generateLogsButton(_ sender: Any) {
+    }
+    
+    /// Displaying our logs data in the UITextView
+    @IBAction func showLogsButton(_ sender: Any) {
+    }
+    
+    /// Action responsible for clearing all the logs history
+    @IBAction func clearLogsButton(_ sender: Any) {
+    }
 }
